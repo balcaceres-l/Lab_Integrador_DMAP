0c67fce4b558bca455787f8456ae00095141558f
Luis Balcaceres <luis.balcaceres@catolica.edu.sv>
2026-07-30 23:19:13 -0600

    Added views for UPDATE AND DELETE buttons

--- lab_integrador/Controllers/ManufacturingProcessesController.cs
+++ lab_integrador/Controllers/ManufacturingProcessesController.cs
@@ -16,7 +16,8 @@ namespace lab_integrador.Controllers
         // GET: ManufacturingProcesses
         public IActionResult Index()
         {
-            return View(_repository.GetAll());
+            var processes = _repository.GetAll();
+            return View(processes);
         }
 
         // GET: ManufacturingProcesses/Create
@@ -30,19 +31,76 @@ namespace lab_integrador.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ManufacturingProcess process)
         {
-            if (ModelState.IsValid)
+            if (_repository.ExistsByName(process.Name))
             {
-                if (_repository.ExistsByName(process.Name))
-                {
-                    ModelState.AddModelError("Name", "Ya existe un proceso con este nombre.");
-                    return View(process);
-                }
+                ModelState.AddModelError(nameof(process.Name), "Ya existe un proceso con este nombre.");
+            }
 
-                _repository.Add(process);
-                TempData["SuccessMessage"] = "Proceso de manufactura creado exitosamente.";
+            if (!ModelState.IsValid)
+            {
+                return View(process);
+            }
+
+            _repository.Add(process);
+            TempData["SuccessMessage"] = "Proceso de fabricación creado exitosamente.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ManufacturingProcesses/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var process = _repository.GetById(id);
+            if (process == null) return NotFound();
+
+            return View(process);
+        }
+
+        // POST: ManufacturingProcesses/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, ManufacturingProcess process)
+        {
+            if (id != process.Id) return NotFound();
+
+            if (_repository.ExistsByName(process.Name, process.Id))
+            {
+                ModelState.AddModelError(nameof(process.Name), "Ya existe otro proceso con este nombre.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(process);
+            }
+
+            _repository.Update(process);
+            TempData["SuccessMessage"] = "Proceso de fabricación actualizado exitosamente.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ManufacturingProcesses/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var process = _repository.GetById(id);
+            if (process == null) return NotFound();
+
+            ViewBag.IsInUse = _repository.IsInUse(id);
+            return View(process);
+        }
+
+        // POST: ManufacturingProcesses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (_repository.IsInUse(id))
+            {
+                TempData["ErrorMessage"] = "No se puede eliminar el proceso porque está asociado a una o más órdenes de producción.";
                 return RedirectToAction(nameof(Index));
             }
-            return View(process);
+
+            _repository.Delete(id);
+            TempData["SuccessMessage"] = "Proceso de fabricación eliminado exitosamente.";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
